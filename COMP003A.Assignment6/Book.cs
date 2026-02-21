4cb54fcef720198e5b3f7c8af355e5b11487549c
NH <noahhulett@my.whccd.edu>
2026-02-20 20:10:24 -0800

    constructor added

--- COMP003A.Assignment6/Book.cs
+++ COMP003A.Assignment6/Book.cs
@@ -5,4 +5,11 @@ public class Book
     public string Title;
     public int Page;
     public bool Finished;
+
+    public Book(string title, int page, bool finished)
+    {
+        Title = title;
+        Page = page;
+        Finished = finished;
+    }
 }
