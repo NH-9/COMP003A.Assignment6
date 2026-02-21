b967ef655ed2c88dffa37f8e8202758872a608ec
NH <noahhulett@my.whccd.edu>
2026-02-20 20:18:54 -0800

    instance methods added

--- COMP003A.Assignment6/Book.cs
+++ COMP003A.Assignment6/Book.cs
@@ -1,15 +1,26 @@
 ﻿using System;
+using System.ComponentModel;
 
 public class Book
 {
     public string Title;
     public int Page;
-    public bool Finished;
+    public bool Complete;
 
-    public Book(string title, int page, bool finished)
+    public Book(string title, int page, bool complete)
     {
         Title = title;
         Page = page;
-        Finished = finished;
+        Complete = complete;
+    }
+
+    public void DisplayValues()
+    {
+        Console.WriteLine($"Title: {Title}, Page Count: {Page}, Completed: {Complete}");
+    }
+
+    public void UpdateValue()
+    {
+        Complete = !Complete;
     }
 }
