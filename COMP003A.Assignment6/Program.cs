083144e18b5778fb88d7f8de050b6efe4951b151
NH <noahhulett@my.whccd.edu>
2026-02-20 20:29:54 -0800

    objects created and menu displayed

--- COMP003A.Assignment6/Program.cs
+++ COMP003A.Assignment6/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Book book1 = new Book("No Longer Human", 177, true);
+            Book book2 = new Book("Thus Spoke Zarathustra", 336, false);
+            Book book3 = new Book("The Divine Comedy", 895, false);
+
+            Console.WriteLine("1. Display all \n2. Update One Item \n3. Exit");
         }
     }
 }
